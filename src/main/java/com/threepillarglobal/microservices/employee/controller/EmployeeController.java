ff07bef8398b26d08cfe347f465a4be6b47c7ac0
diego <diego@diego-VirtualBox>
2016-11-10 17:39:32 +0100

    Initial commit

---
+++ src/main/java/com/threepillarglobal/microservices/employee/controller/EmployeeController.java
@@ -0,0 +1,49 @@
+package com.threepillarglobal.microservices.employee.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.threepillarglobal.microservices.employee.model.Employee;
+import com.threepillarglobal.microservices.employee.repo.EmployeeRepository;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+     
+    @Autowired
+    EmployeeRepository employeeRepository;
+ 
+    @RequestMapping(method = RequestMethod.POST)
+    public Employee create(@RequestBody Employee employee){
+        System.out.println("Entra el el POST");
+        Employee result = new Employee();
+        try{
+        	
+        	result = employeeRepository.save(employee);
+        }catch (Exception e){
+        	e.printStackTrace();
+        }
+        System.out.println("Sale de POST");
+        return result;
+    }
+     
+    @RequestMapping(method = RequestMethod.GET, value="/{employeeId}")
+    public Employee get(@PathVariable String employeeId){
+    	System.out.println("Entra el el GET");
+    	Employee result = new Employee();
+    	try{
+    		
+    		result = employeeRepository.findOne(employeeId);
+    	}catch (Exception e) {
+			e.printStackTrace();
+		}
+    	System.out.println("Sale de GET");
+        return result;
+    }
+     
+     
+}
