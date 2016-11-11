e866a41efc69f17d3dbfea541686b7341f8d26dd
Diego Jarillo <diego.jarillo@gmail.com>
2016-11-11 12:20:31 +0100

    nueva conf

--- src/main/java/com/threepillarglobal/microservices/employee/EmployeeBoot.java
+++ src/main/java/com/threepillarglobal/microservices/employee/EmployeeBoot.java
@@ -2,10 +2,11 @@ package com.threepillarglobal.microservices.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-
+@SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
