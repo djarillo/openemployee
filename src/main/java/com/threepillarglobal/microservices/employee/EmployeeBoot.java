874e4c9274542f1e6133f1a13a7935d8dd7b1cf0
Diego Jarillo <diego.jarillo@gmail.com>
2016-11-14 10:30:59 +0100

    Minor Changes

--- src/main/java/com/threepillarglobal/microservices/employee/EmployeeBoot.java
+++ src/main/java/com/threepillarglobal/microservices/employee/EmployeeBoot.java
@@ -15,8 +15,5 @@ public class EmployeeBoot {
        public static void main(String[] args) {
             SpringApplication.run(EmployeeBoot.class,args);
         }
-     
-//       @Autowired
-//		private Environment env;
 
 }
